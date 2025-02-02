ae3f8d92af1c2fbef192f6f12dc3ea1e509aa8dc
DARVIN KOBE BAJADE <bajade.darvinkobe@gmail.com>
2025-02-02 20:15:58 +0800

    frmMain btn profile creation, frmProfile creation and funtionalities

--- frmEvaluationSup.Designer.cs
+++ frmEvaluationSup.Designer.cs
@@ -28,19 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmEvaluationSup));
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges14 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges15 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges16 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges17 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges18 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges19 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges20 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges23 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges24 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges21 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges22 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             btnReturnEvalSup = new Guna.UI2.WinForms.Guna2Button();
             lblEvalSupMenu = new Label();
             btnConfirmbutton = new Guna.UI2.WinForms.Guna2Button();
@@ -58,7 +58,7 @@
             btnReturnEvalSup.BackColor = Color.CadetBlue;
             btnReturnEvalSup.BorderRadius = 1;
             btnReturnEvalSup.BorderThickness = 2;
-            btnReturnEvalSup.CustomizableEdges = customizableEdges13;
+            btnReturnEvalSup.CustomizableEdges = customizableEdges1;
             btnReturnEvalSup.DisabledState.BorderColor = Color.DarkGray;
             btnReturnEvalSup.DisabledState.CustomBorderColor = Color.DarkGray;
             btnReturnEvalSup.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
@@ -70,7 +70,7 @@
             btnReturnEvalSup.ImageSize = new Size(27, 27);
             btnReturnEvalSup.Location = new Point(645, 23);
             btnReturnEvalSup.Name = "btnReturnEvalSup";
-            btnReturnEvalSup.ShadowDecoration.CustomizableEdges = customizableEdges14;
+            btnReturnEvalSup.ShadowDecoration.CustomizableEdges = customizableEdges2;
             btnReturnEvalSup.Size = new Size(45, 45);
             btnReturnEvalSup.TabIndex = 4;
             // 
@@ -87,7 +87,7 @@
             // 
             // btnConfirmbutton
             // 
-            btnConfirmbutton.CustomizableEdges = customizableEdges15;
+            btnConfirmbutton.CustomizableEdges = customizableEdges3;
             btnConfirmbutton.DisabledState.BorderColor = Color.DarkGray;
             btnConfirmbutton.DisabledState.CustomBorderColor = Color.DarkGray;
             btnConfirmbutton.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
@@ -97,7 +97,7 @@
             btnConfirmbutton.ForeColor = Color.White;
             btnConfirmbutton.Location = new Point(510, 446);
             btnConfirmbutton.Name = "btnConfirmbutton";
-            btnConfirmbutton.ShadowDecoration.CustomizableEdges = customizableEdges16;
+            btnConfirmbutton.ShadowDecoration.CustomizableEdges = customizableEdges4;
             btnConfirmbutton.Size = new Size(180, 45);
             btnConfirmbutton.TabIndex = 6;
             btnConfirmbutton.Text = "Confirm";
@@ -106,7 +106,7 @@
             // cmbEmployeeEval
             // 
             cmbEmployeeEval.BackColor = Color.Transparent;
-            cmbEmployeeEval.CustomizableEdges = customizableEdges17;
+            cmbEmployeeEval.CustomizableEdges = customizableEdges5;
             cmbEmployeeEval.DrawMode = DrawMode.OwnerDrawFixed;
             cmbEmployeeEval.DropDownStyle = ComboBoxStyle.DropDownList;
             cmbEmployeeEval.FocusedColor = Color.FromArgb(94, 148, 255);
@@ -116,7 +116,7 @@
             cmbEmployeeEval.ItemHeight = 30;
             cmbEmployeeEval.Location = new Point(175, 138);
             cmbEmployeeEval.Name = "cmbEmployeeEval";
-            cmbEmployeeEval.ShadowDecoration.CustomizableEdges = customizableEdges18;
+            cmbEmployeeEval.ShadowDecoration.CustomizableEdges = customizableEdges6;
             cmbEmployeeEval.Size = new Size(331, 36);
             cmbEmployeeEval.TabIndex = 7;
             cmbEmployeeEval.SelectedIndexChanged += cmbEmployeeEval_SelectedIndexChanged;
@@ -124,7 +124,7 @@
             // cmbTaskGiven
             // 
             cmbTaskGiven.BackColor = Color.Transparent;
-            cmbTaskGiven.CustomizableEdges = customizableEdges19;
+            cmbTaskGiven.CustomizableEdges = customizableEdges7;
             cmbTaskGiven.DrawMode = DrawMode.OwnerDrawFixed;
             cmbTaskGiven.DropDownStyle = ComboBoxStyle.DropDownList;
             cmbTaskGiven.FocusedColor = Color.FromArgb(94, 148, 255);
@@ -134,7 +134,7 @@
             cmbTaskGiven.ItemHeight = 30;
             cmbTaskGiven.Location = new Point(175, 283);
             cmbTaskGiven.Name = "cmbTaskGiven";
-            cmbTaskGiven.ShadowDecoration.CustomizableEdges = customizableEdges20;
+            cmbTaskGiven.ShadowDecoration.CustomizableEdges = customizableEdges8;
             cmbTaskGiven.Size = new Size(331, 36);
             cmbTaskGiven.TabIndex = 9;
             // 
@@ -153,30 +153,30 @@
             // 
             pnlEmployee.Controls.Add(cmbTaskGivenEmployee);
             pnlEmployee.Controls.Add(label2);
-            pnlEmployee.CustomizableEdges = customizableEdges23;
-            pnlEmployee.Location = new Point(608, 99);
+            pnlEmployee.CustomizableEdges = customizableEdges11;
+            pnlEmployee.Location = new Point(661, 89);
             pnlEmployee.Name = "pnlEmployee";
-            pnlEmployee.ShadowDecoration.CustomizableEdges = customizableEdges24;
+            pnlEmployee.ShadowDecoration.CustomizableEdges = customizableEdges12;
             pnlEmployee.Size = new Size(465, 304);
             pnlEmployee.TabIndex = 10;
             pnlEmployee.Visible = false;
             // 
             // cmbTaskGivenEmployee
             // 
-            cmbTaskGivenEmployee.BackColor = Color.Transparent;
-            cmbTaskGivenEmployee.CustomizableEdges = customizableEdges21;
-            cmbTaskGivenEmployee.DrawMode = DrawMode.OwnerDrawFixed;
-            cmbTaskGivenEmployee.DropDownStyle = ComboBoxStyle.DropDownList;
-            cmbTaskGivenEmployee.FocusedColor = Color.FromArgb(94, 148, 255);
-            cmbTaskGivenEmployee.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
-            cmbTaskGivenEmployee.Font = new Font("Segoe UI", 10F);
-            cmbTaskGivenEmployee.ForeColor = Color.FromArgb(68, 88, 112);
-            cmbTaskGivenEmployee.ItemHeight = 30;
-            cmbTaskGivenEmployee.Location = new Point(83, 147);
-            cmbTaskGivenEmployee.Name = "cmbTaskGivenEmployee";
-            cmbTaskGivenEmployee.ShadowDecoration.CustomizableEdges = customizableEdges22;
-            cmbTaskGivenEmployee.Size = new Size(331, 36);
-            cmbTaskGivenEmployee.TabIndex = 12;
+            guna2ComboBox3.BackColor = Color.Transparent;
+            guna2ComboBox3.CustomizableEdges = customizableEdges9;
+            guna2ComboBox3.DrawMode = DrawMode.OwnerDrawFixed;
+            guna2ComboBox3.DropDownStyle = ComboBoxStyle.DropDownList;
+            guna2ComboBox3.FocusedColor = Color.FromArgb(94, 148, 255);
+            guna2ComboBox3.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2ComboBox3.Font = new Font("Segoe UI", 10F);
+            guna2ComboBox3.ForeColor = Color.FromArgb(68, 88, 112);
+            guna2ComboBox3.ItemHeight = 30;
+            guna2ComboBox3.Location = new Point(83, 147);
+            guna2ComboBox3.Name = "guna2ComboBox3";
+            guna2ComboBox3.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            guna2ComboBox3.Size = new Size(331, 36);
+            guna2ComboBox3.TabIndex = 12;
             // 
             // label2
             // 
@@ -202,7 +202,6 @@
             Controls.Add(cmbEmployeeEval);
             Controls.Add(btnConfirmbutton);
             Controls.Add(lblEvalSupMenu);
-            Controls.Add(btnReturnEvalSup);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "frmEvaluationSup";
@@ -216,8 +215,6 @@
         }
 
         #endregion
-
-        private Guna.UI2.WinForms.Guna2Button btnReturnEvalSup;
         private Label lblEvalSupMenu;
         private Guna.UI2.WinForms.Guna2Button btnConfirmbutton;
         private Guna.UI2.WinForms.Guna2ComboBox cmbEmployeeEval;
